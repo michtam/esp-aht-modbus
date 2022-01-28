dfa450f0d752cffcb4157479fce425fe77d3e71a
michtam <12645951+michtam@users.noreply.github.com>
2022-01-28 17:25:34 +0100

    Init commit with src

---
+++ src/registers.h
@@ -0,0 +1,9 @@
+const int modbus_registers[] = {
+  //ADDR     REGISTER NAME
+  0x0000, // TEMPERATURE
+  0x0001, // HUMIDITY
+  0x0002, // HEATER STATUS
+  0x0003, // 
+  0x0004, // SENSOR STATUS
+  0x0200  // HEATER ENABLED
+};
